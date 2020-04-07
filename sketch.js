39d63d1a2903622c2b4cedceadeb901811e65cdf
pratyushcreate <pratyushhh04@gmail.com>
2020-04-07 10:19:05 +0530

    colour

--- sketch.js
+++ sketch.js
@@ -15,14 +15,5 @@ function setup(){
 
 function draw(){
 
-  // change the value of Red based on the mouse movement about the X axis
-  // change the value of Green based on the mouse movement about the X axis
-  // change the value of Blue based on the mouse movement about the X axis
-
-  // Use the map() function to do so. 
-
-  // Pass the values to the background() function you have learnt previously.
-
-  // Create an ellipse that will move around with you mouse about the X axis.
-  // Remember to fill the canvas with white paint before creating the ellipse.
+  backgroundcolour = ("green");
 }
